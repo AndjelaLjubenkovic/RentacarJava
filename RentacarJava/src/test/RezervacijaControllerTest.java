69245933de0abbf57243058f54f8fffb4e86ffdf
AndjelaLjubenkovic <andjela.ljubenkovic@sistemi.rs>
2023-06-08 16:53:34 +0200

    added test methods

--- RentacarJava/src/test/RezervacijaControllerTest.java
+++ RentacarJava/src/test/RezervacijaControllerTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import controller.RezervacijaController;
+import controller.AutoController;
 import konekcija.DBConnection;
 import model.Rezervacija;
+import model.Auto;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -16,6 +19,7 @@ import java.util.List;
 public class RezervacijaControllerTest {
     
 	private RezervacijaController rezervacijaController;
+	private AutoController autoController = new AutoController();
     @BeforeEach
     public void setUp() {
         rezervacijaController = new RezervacijaController();
@@ -36,7 +40,37 @@ public class RezervacijaControllerTest {
         List<Rezervacija> rezervacije = rezervacijaController.dobaviSveRezervacije();
         assertTrue(rezervacije.contains(rezervacija));
     }
-
+    public void testAzurirajRezervaciju()
+    {
+    	int rezervacijaId = 35;
+    	Rezervacija rezervacija = rezervacijaController.dobaviRezervaciju(rezervacijaId);
+    	 Auto prethodniAuto = autoController.dobaviAuto(rezervacija.getAuto_id());
+    	 prethodniAuto.setIznajmljen(false);
+    	 autoController.azurirajAuto(prethodniAuto);
+    	 assertFalse(prethodniAuto.isIznajmljen());
+    	rezervacija.setAuto_id(6);
+    	rezervacija.setKlijent_id(11);
+    	rezervacijaController.azurirajRezervaciju(rezervacija);
+    	Rezervacija azuriranaRezervacija = rezervacijaController.dobaviRezervaciju(rezervacijaId);
+    	 assertEquals(rezervacija.getAuto_id(),azuriranaRezervacija.getAuto_id());
+    	 assertEquals(rezervacija.getKlijent_id(),azuriranaRezervacija.getKlijent_id());
+    	 Auto auto = autoController.dobaviAuto(6);
+    	 auto.setIznajmljen(true);
+    	 autoController.azurirajAuto(auto);
+    	 assertTrue(auto.isIznajmljen());
+    	
+    } 
+    /**
+     * Metoda koja testira brisanje rezervacije automobila u bazi
+     */
+     @Test
+     public void testObrisiRezervaciju()
+     {
+   	  int rezervacijaId = 36;
+   	  rezervacijaController.obrisiRezervaciju(rezervacijaId);
+   	  List<Rezervacija> rezervacije = rezervacijaController.dobaviSveRezervacije();
+   	  assertFalse(rezervacije.stream().anyMatch(rezervacija -> rezervacija.getRezervacija_id() == rezervacijaId));
+     }
     /**
      * Metoda koja testira dobavljanje svih rezervacija iz baze
      */
